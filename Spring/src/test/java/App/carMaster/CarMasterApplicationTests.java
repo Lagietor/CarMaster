377be5f00c8a1b2aa28ba81254b1781956bcd776
Lagietor <pawelwiszniewski4@interia.pl>
2023-10-08 15:27:07 +0200

    code initialization

---
+++ Spring/src/test/java/App/carMaster/CarMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package App.carMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
